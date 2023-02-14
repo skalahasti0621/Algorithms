c6cefd930c49104b6051655cbd4b1ee00c50ae5d
Sreenath Kalahasti <sreenath.kalahasti@gmail.com>
2023-02-14 01:28:45 -0800

    Update IsStringPalindrome.py

--- IsStringPalindrome.py
+++ IsStringPalindrome.py
@@ -5,14 +5,15 @@
 def isPalindrome(str,i):
     #str="abba"
     #print(str[len(str)-1])
-    if str[i]!=str[len(str)-1]:
+    if str[i]!=str[len(str)-i-1]:
         return False 
     if (i >=len(str)/2):
         return True
     return isPalindrome(str,i+1)
 
-str="abba"
-print(isPalindrome(str,len(str)-1))
+#Testing Purpose: 
+#str="abba"
+#print(isPalindrome(str,0))
 
-# str="abbac"
-# print(isPalindrome(str,0,len(str)-1))
+#str="abbac"
+#print(isPalindrome(str,0))
